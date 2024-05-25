062f43989b4ba04fefb79d1f56ab6ef28c1bf9eb
Niklas Eicker <git@nikl.me>
2024-05-25 12:07:29 +0200

    Audio and animation

--- src/animation.rs
+++ src/animation.rs
@@ -1,1 +1,34 @@
+use bevy::prelude::*;
 
+pub struct SpriteAnimationPlugin;
+
+impl Plugin for SpriteAnimationPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, animate_sprite);
+    }
+}
+
+#[derive(Component)]
+pub struct AnimationIndices {
+    pub(crate) first: usize,
+    pub(crate) last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+pub struct AnimationTimer(pub Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut to_animate: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlas)>,
+) {
+    for (indices, mut timer, mut atlas) in &mut to_animate {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            atlas.index = if atlas.index == indices.last {
+                indices.first
+            } else {
+                atlas.index + 1
+            };
+        }
+    }
+}
--- src/audio.rs
+++ src/audio.rs
@@ -1,1 +1,16 @@
+use bevy::prelude::*;
 
+pub struct InternalAudioPlugin;
+
+impl Plugin for InternalAudioPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, start_background);
+    }
+}
+
+fn start_background(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(AudioBundle {
+        source: asset_server.load("Windless Slopes.ogg"),
+        settings: PlaybackSettings::LOOP,
+    });
+}
